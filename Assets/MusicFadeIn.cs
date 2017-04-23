3518a27b4493c0edd5f1c4d38b8a867c5ea3e2f7
Josh Maggard <jmaggard62@gmail.com>
2017-04-23 10:51:22 -0500

    Shorter song

---
+++ Assets/MusicFadeIn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicFadeIn : MonoBehaviour {
+
+    AudioSource aSource;
+	// Use this for initialization
+	void Start () {
+        aSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
